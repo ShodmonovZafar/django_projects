817716e8587eafc4b2fc0e97cada7786f8b390df
ShodmonovZafar <uzbekdasturchisiman@gmail.com>
2023-05-13 11:43:14 +0500

    z

--- project_3/config/settings.py
+++ project_3/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-rhu@ix&gbk$(4mwzadu!*=+u$uvaf@%uohb*)m$#%*m7-znv8!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
